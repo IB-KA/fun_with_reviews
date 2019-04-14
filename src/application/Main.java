24c93ea263bb0287043236b77376cfa6ad5df5d1
IB <inga@batton.de>
2019-04-14 12:57:01 +0200

    something to see

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,20 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			AnchorPane root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("Diceroller");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
